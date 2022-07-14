8f367ad7a3fe33a37b94319bb83905fa074c6c5b
Katie Moar <katiemoar@hotmail.com>
2022-07-13 22:29:29 -0500

    addition: sum-of-odd.js

---
+++ js/sum-of-odd.js
@@ -0,0 +1,32 @@
+/* -------------------------------------------- */
+/*                   QUESTION                   */
+/* -------------------------------------------- */
+
+// Given the triangle of consecutive odd numbers:
+
+//              1
+//           3     5
+//        7     9    11
+//    13    15    17    19
+// 21    23    25    27    29
+// ...
+// Calculate the sum of the numbers in the nth row of this triangle (starting at index 1) e.g.: (Input --> Output)
+
+// 1 -->  1
+// 2 --> 3 + 5 = 8
+
+/* -------------------------------------------- */
+/*                    ANSWER                    */
+/* -------------------------------------------- */
+
+function rowSumOddNumbers(n) {
+    console.log(Math.pow(n, 3))
+  }
+
+rowSumOddNumbers(3)
+
+/* -------------------------------------------- */
+/*                     NOTES                    */
+/* -------------------------------------------- */
+
+//This was mathy and question didn't sink in. Had to look at answer. 
