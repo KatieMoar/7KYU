64e9f1f917dbd3d07418640d2ad9917103d66c0a
Katie Moar <katiemoar@hotmail.com>
2022-10-23 20:43:22 -0500

    addition:two-fighters-one-winner.js

---
+++ js/two-fighters-one-winner.js
@@ -0,0 +1,88 @@
+/* -------------------------------------------- */
+/*                   QUESTION                   */
+/* -------------------------------------------- */
+
+// Create a function that returns the name of the winner in a fight between two fighters.
+
+// Each fighter takes turns attacking the other and whoever kills the other first is victorious. Death is defined as having health <= 0.
+
+// Each fighter will be a Fighter object/instance. See the Fighter class below in your chosen language.
+
+// Both health and damagePerAttack (damage_per_attack for python) will be integers larger than 0. You can mutate the Fighter objects.
+
+// Your function also receives a third argument, a string, with the name of the fighter that attacks first.
+
+// Example:
+//   declare_winner(Fighter("Lew", 10, 2), Fighter("Harry", 5, 4), "Lew") => "Lew"
+  
+//   Lew attacks Harry; Harry now has 3 health.
+//   Harry attacks Lew; Lew now has 6 health.
+//   Lew attacks Harry; Harry now has 1 health.
+//   Harry attacks Lew; Lew now has 2 health.
+//   Lew attacks Harry: Harry now has -1 health and is dead. Lew wins.
+// function Fighter(name, health, damagePerAttack) {
+//         this.name = name;
+//         this.health = health;
+//         this.damagePerAttack = damagePerAttack;
+//         this.toString = function() { return this.name; }
+// }
+
+
+
+/* -------------------------------------------- */
+/*                    ANSWER                    */
+/* -------------------------------------------- */
+
+function Fighter(name, health, damagePerAttack) {
+    this.name = name;
+    this.health = health;
+    this.damagePerAttack = damagePerAttack;
+    this.toString = function() { return this.name; }
+}
+
+function declareWinner(fighter1, fighter2, firstAttacker) {
+    if(fighter1.name === firstAttacker){
+        if(fighter2.health > 0){}
+    }
+    console.log(fighter1)
+    console.log(fighter2)
+    console.log(firstAttacker)
+  }
+
+
+  function declareWinner(fighter1, fighter2, firstAttacker) {
+    while (fighter1.health > 0 && fighter2.health > 0) {
+      fighter2.health -= fighter1.damagePerAttack;
+      fighter1.health -= fighter2.damagePerAttack;
+    }
+    
+    if (fighter1.health <= 0 && fighter2.health <= 0)
+      return firstAttacker;
+    else if (fighter1.health <= 0)
+      return fighter2.name;
+    else
+      return fighter1.name;
+  }
+
+/* -------------------------------------------- */
+/*                  TEST CASES                  */
+/* -------------------------------------------- */
+declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Lew") //"Lew");
+
+// declareWinner(new Fighter("Lew", 10, 2), new Fighter("Harry", 5, 4), "Harry") // "Harry");
+
+// declareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4), "Harry") //"Harald")
+
+// declareWinner(new Fighter("Harald", 20, 5), new Fighter("Harry", 5, 4), "Harald") //"Harald")
+
+// declareWinner(new Fighter("Jerry", 30, 3), new Fighter("Harald", 20, 5), "Jerry") // "Harald")
+
+// declareWinner(new Fighter("Jerry", 30, 3), new Fighter("Harald", 20, 5), "Harald") // "Harald")
+
+
+//PREP
+//Parameters: Taking in two fighters and who attacks first
+//Return: The winner of the fight
+//Name, Health, Damage
+
+//COULDN'T GET THIS ONE, HAD A LONG DAY. I ALMOST GOT THERE THO.
