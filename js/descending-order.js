b5ffae9a5c5ad6762769b1206ad6924c4228360e
Katie Moar <katiemoar@hotmail.com>
2022-12-15 21:32:03 -0600

    addition: descending-order.js

---
+++ js/descending-order.js
@@ -0,0 +1,31 @@
+/* -------------------------------------------- */
+/*                   QUESTION                   */
+/* -------------------------------------------- */
+// Your task is to make a function that can take any non-negative integer as an argument and return it with its digits in descending order. Essentially, rearrange the digits to create the highest possible number.
+
+// Examples:
+// Input: 42145 Output: 54421
+
+// Input: 145263 Output: 654321
+
+// Input: 123456789 Output: 987654321
+
+
+/* -------------------------------------------- */
+/*                    ANSWER                    */
+/* -------------------------------------------- */
+function descendingOrder(n){
+    let str = n.toString().split('').sort(function (a,b) {return b-a}).join('')
+    return Number(str)
+  }
+
+
+/* -------------------------------------------- */
+/*                  TEST CASES                  */
+/* -------------------------------------------- */
+// descendingOrder(0)   // 0)
+// descendingOrder(1)   // 1)
+// descendingOrder(111)   // 111)
+// descendingOrder(15)   // 51)
+descendingOrder(1021)   // 2110)
+// descendingOrder(123456789)   // 987654321)
